316bb84a61f9af975da3ed0255a54c994bc79415
Takashi Kunihiro <t27.kunihiro@gmail.com>
2020-10-05 23:55:15 +0900

    create scrape rss

---
+++ scrape_rss_by_feedparser.py
@@ -0,0 +1,9 @@
+import feedparser
+
+# はてなブックマークの人気エントリー(テクノロジー)のRSSを読み込む
+d = feedparser.parse('http://b.hatena.ne.jp/hotentry/it.rss')
+
+# 全ての要素について処理を繰り返す。
+for entry in d.entries:
+    print(entry.link, entry.title) # URLとタイトルを出力する
+
