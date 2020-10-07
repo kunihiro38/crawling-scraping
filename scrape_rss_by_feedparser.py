b201c4341c4c2715c244e63f25486a97c45cc054
Takashi Kunihiro <t27.kunihiro@gmail.com>
2020-10-07 21:05:53 +0900

    use mysql and save db

--- scrape_rss_by_feedparser.py
+++ scrape_rss_by_feedparser.py
@@ -1,3 +1,4 @@
+'''feedparser'''
 import feedparser
 
 # はてなブックマークの人気エントリー(テクノロジー)のRSSを読み込む
@@ -6,4 +7,3 @@ d = feedparser.parse('http://b.hatena.ne.jp/hotentry/it.rss')
 # 全ての要素について処理を繰り返す。
 for entry in d.entries:
     print(entry.link, entry.title) # URLとタイトルを出力する
-
