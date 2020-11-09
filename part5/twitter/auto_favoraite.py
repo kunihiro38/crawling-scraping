ba1f62b12fe97fd9afb684d77656bedee3c7814e
Takashi Kunihiro <t27.kunihiro@gmail.com>
2020-11-09 21:36:32 +0900

    twitter text

--- part5/twitter/auto_favoraite.py
+++ part5/twitter/auto_favoraite.py
@@ -9,10 +9,10 @@ def remove_emoji(src_str):
 
 
 # 取得した各種キーを格納-----------------------------------------------------
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = "ADszgSxou1TsAD076fuslsLEG"
+CONSUMER_SECRET = "RN3yhLzJHOnqPUfURXEFCQxU9D8teaWLMVMwZqL7JjD6bxTii6"
+ACCESS_TOKEN = "953993444099219456-WaIffLmlu3LfHxVMiDQhG7h8XCXIOHv"
+ACCESS_TOKEN_SECRET = "FksrWC9znitAMnOHrKGy0hZ9Rcw2xAvAhod3SG1iPtvXs"
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -21,11 +21,11 @@ api = tweepy.API(auth)
 
 # 取得したいキーワード
 search_list = ['#駆け出しエンジニア', '#python']
-search_list = ['#今日の積み上げ']
+search_list = ['#今日の積み上げ', '#駆け出しエンジニア']
 
 # ツイート件数を入れた数だけいいねする
 # ちなみにいいねは24時間で1000件が上限で、それを超えるとペナルティ(アカウント停止)を受ける
-tweet_count = 100
+tweet_count = 50
 
 for search in search_list:
     # サーチ結果
@@ -35,7 +35,7 @@ for search in search_list:
         try:
             api.create_favorite(id=tweet_id)
             print('いいね!!をしました')
-            time.sleep(60)
+            time.sleep(5)
         except tweepy.TweepError as e:
             print(e.reason)
         except StopIteration:
---
+++ part5/twitter/twitter-wordcloud.py
@@ -0,0 +1,76 @@
+# 参考 http://docs.tweepy.org/en/latest/
+
+import tweepy
+import time
+import MeCab
+import numpy
+import matplotlib.pyplot as plt
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS # 単語の頻出頻度の可視化
+
+
+# 絵文字を除去する
+def remove_emoji(src_str):
+    return ''.join(c for c in src_str if ctnot in emoji.UNiCODE_EMOJI)
+
+
+# 取得した各種キーを格納-----------------------------------------------------
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+Account = "Hiroyan_python"
+tweets = api.user_timeline(Account, count=200, page=1)
+num = 1
+text = ''
+# for tweet in tweets:
+#     print('twid : ', tweet.id)               # tweetのID
+#     print('user : ', tweet.user.screen_name)  # ユーザー名
+#     print('date : ', tweet.created_at)      # 呟いた日時
+#     print(tweet.text)                  # ツイート内容
+#     print('favo : ', tweet.favorite_count)  # ツイートのいいね数
+#     print('retw : ', tweet.retweet_count)  # ツイートのリツイート数
+#     print('ツイート数 : ', num) # ツイート数
+#     print('='*80) # =を80個表示
+#     num += 1 # ツイート数を計算
+#     list += tweet.text
+
+for tweet in tweets:
+    text += tweet.text
+
+# 改行除去
+j = " ".join([i.split("\t")[0] for i in text.split("\n")])
+print(j)
+
+# 含めたくないワードを追加
+add_STOPWORDS = ["さん","この","たち","総理","まし","です","ます","から","いる","ない","する","ある","なる","れる","できる","これ","こと","さん","られる","やる","てる","ませ","その"] #表示させないキーワードを追加する
+for word in add_STOPWORDS:
+    STOPWORDS.add(word)
+
+
+str(STOPWORDS)
+st_text = text.encode("utf-8")
+tagger = MeCab.Tagger("-Ochasen")
+message = tagger.parse(text)
+print(j)
+
+# word
+# wordcloud = WordCloud(
+#     width=480,
+#     height=320,
+#     background_color = "white",
+#     max_words = 2000,
+#     font_path = "./hirakakupro-w4.otf" 
+# )
+
+# wordcloud.generate(j)
+# wordcloud.to_file('sample.jpg')
+# file = open('sample.jpg')
+
+
